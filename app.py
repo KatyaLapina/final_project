6d2a9bb34141da97be57ff4235f0a819dcf18ca3
KatyaLapina <st077070@student.spbu.ru>
2024-06-14 11:15:15 +0300

    Update app.py
    final_commit

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, request
-
+import numpy
+import joblib
+MODEL_PATH ='/home/lapina/mlmodel/finalized_model.sav' 
 app = Flask(__name__)
 
+model = joblib.load(MODEL_PATH)
 @app.route('/predict_price', methods = ['GET'])
 def predict():
     args = request.args
-    open_plan  =  args.get('open_plan', default = -1, type = int)
     rooms = args.get('rooms', default = -1, type = int)
     area  = args.get('area', default = -1, type = float)
     rennovation = args.get('rennovation', default = -1, type = int)
+    floor = args.get('floor', default = -1, type = int)
+    x = numpy.array([rooms, area, rennovation, floor]).reshape(1,-1)
 
-    response = "open plan {}, rooms:{}, area:{}, rennovation:{}".format(open_plan, rooms, area, rennovation)
-
-    return response
+    result = model.predict(x)
+    result = result.reshape(1,-1)
+    return str(result[0][0])
 if __name__ == '__main__':
-    app.run(debug=True, port=5444, host='0.0.0.0')
+    app.run(debug=True, port=7778, host='0.0.0.0')
